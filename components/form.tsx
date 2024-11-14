908f5715791e73153b520fee07ea55ad3f915503
DARSAN <iamspdarsan@gmail.com>
2024-11-14 05:42:23 +0530

    form added in front-end

---
+++ components/form.tsx
@@ -0,0 +1,84 @@
+import { Button, Fieldset, Group, TextInput } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { LuPlus } from "react-icons/lu";
+
+import postman from "@/scripts/postman";
+import { Modal } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+
+export default () => {
+	const [opened, { open, close }] = useDisclosure(false);
+	const isMobile = useMediaQuery("(max-width: 50em)");
+
+	return (
+		<>
+			<Modal
+				opened={opened}
+				onClose={close}
+				fullScreen={isMobile}
+				transitionProps={{ transition: "fade", duration: 200 }}
+				radius={"xl"}>
+				<form
+					method="post"
+					onSubmit={(event) => {
+						postman(event, close);
+					}}>
+					<Fieldset legend="Enter book details here">
+						<TextInput
+							required
+							placeholder="Book title"
+							label="Title"
+							style={{ flex: 1 }}
+							type="text"
+							name="title"
+						/>
+						<TextInput
+							mt={"sm"}
+							required
+							label="Author"
+							placeholder="Author name"
+							style={{ flex: 1 }}
+							type="text"
+							name="author"
+						/>
+						<TextInput
+							required
+							mt={"sm"}
+							label="Description"
+							placeholder="Brief description"
+							name="desc"
+							type="text"
+							style={{ flex: 1 }}
+						/>
+						<TextInput
+							mt={"sm"}
+							required
+							label="Thumbnail link"
+							placeholder="Cover image link"
+							name="thumbnail"
+							type="url"
+							style={{ flex: 1 }}
+						/>
+					</Fieldset>
+					<Group
+						justify="flex-end"
+						mt="md">
+						<Button
+							color="#f00"
+							type="submit">
+							Submit
+						</Button>
+					</Group>
+				</form>
+			</Modal>
+
+			<Button
+				color="#f00"
+				onClick={open}
+				radius={"xl"}>
+				<span className="hidden sm:inline">Add Book</span>{" "}
+				<LuPlus className="w-7 h-7 sm:w-5 sm:h-5" />
+			</Button>
+		</>
+	);
+};
---
+++ scripts/postman.ts
@@ -0,0 +1,34 @@
+import { FormEvent } from "react";
+
+export default function postman(
+	event: FormEvent<HTMLFormElement>,
+	callback: () => void = () => {},
+) {
+	event.preventDefault();
+
+	const formData = new FormData(event.currentTarget);
+
+	const data = Object.fromEntries(formData.entries()); // Convert FormData to a plain object
+
+	const endpoint = "http://localhost:9080/books";
+
+	fetch(endpoint, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	})
+		.then((response) => response.json())
+		.then(({ success }) => {
+			if (success) {
+				callback();
+			} else {
+				alert("Try again!");
+			}
+		})
+		.catch((error) => {
+			alert("Try again!");
+			console.error("Error:", error);
+		});
+}
